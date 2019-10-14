43496eb7a952fbec5ec9968e80d66575136d7eb2
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-10-14 17:17:14 +0200

    Init the module

---
+++ lambda.tf
@@ -0,0 +1,83 @@
+data "aws_iam_policy_document" "role_policy" {
+  statement {
+    sid = "LambdaAssumeRole"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com",
+      ]
+    }
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.default_account_arn,
+        var.security_account_arn,
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = local.lambda_function_name
+  assume_role_policy = data.aws_iam_policy_document.role_policy.json
+}
+
+# Policies Attachment
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = data.aws_iam_policy.aws_lambda_vpc_access.arn
+}
+
+resource "aws_lambda_function" "lambda" {
+  depends_on = [
+    aws_iam_role.lambda,
+    aws_cloudwatch_log_group.lambda
+  ]
+
+  function_name    = local.lambda_function_name
+  source_code_hash = filebase64sha256(var.lambda_filepath)
+
+  publish = true
+
+  s3_bucket = data.aws_s3_bucket.deployment_bucket.id
+  s3_key    = var.lambda_bucket_key
+
+  handler = "functions/migrate.handler"
+  runtime = "nodejs10.x"
+
+  timeout     = 60
+  memory_size = 1024
+
+  vpc_config {
+    subnet_ids         = var.vpc["subnet_ids"]
+    security_group_ids = var.vpc["security_group_ids"]
+  }
+
+  tags = "${var.common_tags}"
+
+  environment {
+    variables = {
+      STAGE = var.stage
+    }
+  }
+
+  role = aws_iam_role.lambda.arn
+}
+
+# Call the lambda function
+data "aws_lambda_invocation" "run_lambda" {
+  count         = var.enable_invocation ? 1 : 0
+  depends_on    = [aws_lambda_function.lambda]
+  function_name = "${aws_lambda_function.lambda.function_name}"
+
+  input = <<JSON
+{}
+JSON
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "stage" {
+  type        = "string"
+  description = "The deploy environment in [prod, dev, preprod]"
+}
+
+variable "common_tags" {
+  type        = "map"
+  description = "a list of tags set on the different resources"
+  default     = {}
+}
+
+variable "app_id" {
+  type        = "string"
+  description = "The id of the application"
+}
+
+variable "app_name" {
+  type        = "string"
+  description = "The name of the application"
+}
+
+variable "vpc" {
+  type        = "map"
+  description = "expected subnet_ids => [] and security_group_ids => []"
+}
+
+variable "security_account_arn" {
+  type        = "string"
+  description = "The security account arn"
+}
+
+variable "default_account_arn" {
+  type        = "string"
+  description = "The security account arn"
+}
+
+variable "lambda_filepath" {
+  type        = "string"
+  description = "the path to the lambda.zip file"
+}
+
+variable "lambda_cannonicalname" {
+  type        = "string"
+  description = "the name used in computation of the lambda name - <stage>-<app_name>-<cannonical_name>"
+}
+
+variable "lambda_handler" {
+  type        = "string"
+  description = "the handler called by the lambda"
+}
+
+variable "lambda_bucket" {
+  type        = "string"
+  description = "the bucket containing the lambda function"
+}
+
+variable "lambda_bucket_key" {
+  type        = "string"
+  description = "the path to the lambda.zip file"
+}
+
+variable "enable_invocation" {
+  type        = bool
+  default     = false
+  description = "whether the lambda is invocated after creation"
+}
+
+variable "lambda_logs_to_kibana_name" {
+  type        = "string"
+  description = "name of the lambda to log in kibana"
+}
+
+variable "logs_to_kibana_subscription_filter_name" {
+  type        = "string"
+  description = "the kibana subscription filter name"
+}
+
+
+variable "secret_managers" {
+  type        = "list"
+  description = "list of the secret manager the lambda can read"
+  default     = []
+}
